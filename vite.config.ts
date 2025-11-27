2856de7e0a065c8cd7e4937c8a7ba8b3eecf51fe
eduardo12 <eduardo.salcedo.aya@gmail.com>
2025-11-27 10:48:11 -0500

    cambiosss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/premioPuerta/',  // ← CAMBIA esto por el nombre EXACTO de tu repo
 })
